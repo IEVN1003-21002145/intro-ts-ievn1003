2d40f41ba02808c078e4d26b3858c1681dd4e898
Guillermo Chavez <21002145@alumnos.utleon.edu.mx>
2025-09-05 06:49:37 -0600

    Segundo commit 05-09-2025

---
+++ src/temasTS/01-tipos-vas.ts
@@ -0,0 +1,19 @@
+console.log("Hola mundo");
+
+var num1=23;
+let num2=45;
+const num3=67;
+
+let nombre:string="David";
+let num:number=23;
+let activo:boolean;
+
+let sum = num1 + num2 + num3;
+console.log("La suma es: "+sum);
+
+let matricula:number|string;
+
+matricula=21213243;
+matricula="WAWA21213243";
+
+console.log("Matrícula: "+matricula);
